8b12f3d85f2f615d5fd8186c6916efa744568e9c
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2010-08-10 09:58:11 +0200

    some work toward checking things in with fssync

--- src/gocept/fssyncz2/__init__.py
+++ src/gocept/fssyncz2/__init__.py
@@ -1,8 +1,13 @@
 import Missing
+import cgi
 import pickle
 import zope.app.fssync.browser
+import zope.app.fssync.syncer
+import zope.component
 import zope.fssync.interfaces
+import zope.fssync.repository
 import zope.fssync.task
+import zope.interface
 import zope.security.proxy
 
 
@@ -25,6 +30,33 @@ class SnarfFile(zope.app.fssync.browser.SnarfFile):
         return super(SnarfFile, self).show().read()
 
 
+class SnarfCommit(zope.app.fssync.browser.SnarfCheckin):
+
+    def check_content_type(self):
+        if not self.request.get_header("Content-Type") == "application/x-snarf":
+            raise ValueError(_("Content-Type is not application/x-snarf"))
+
+    def parse_args(self):
+        # The query string in the URL didn't get parsed, because we're
+        # getting a POST request with an unrecognized content-type
+        qs = self.request.environ.get("QUERY_STRING")
+        if qs:
+            self.args = cgi.parse_qs(qs)
+        else:
+            self.args = {}
+
+
+class SnarfCheckin(SnarfCommit):
+
+    def run_submission(self):
+        stream = self.request.stdin
+        snarf = zope.fssync.repository.SnarfRepository(stream)
+        checkin = zope.fssync.task.Checkin(
+            zope.app.fssync.syncer.getSynchronizer, snarf)
+        checkin.perform(self.container, self.name, self.fspath)
+        return ""
+
+
 class InsecureCheckout(zope.fssync.task.Checkout):
 
     def dump(self, synchronizer, path):
@@ -45,3 +77,15 @@ class InsecureCheckout(zope.fssync.task.Checkout):
 
 
 zope.fssync.task.Checkout = InsecureCheckout
+
+
+@zope.component.adapter(zope.interface.Interface)
+@zope.interface.implementer(zope.fssync.interfaces.IEntryId)
+def EntryId(obj):
+    try:
+        path = obj.absolute_url_path()
+        return path.encode('utf-8')
+    except (TypeError, KeyError, AttributeError):
+        # this case can be triggered for persistent objects that don't
+        # have a name in the content space (annotations, extras)
+        return None
