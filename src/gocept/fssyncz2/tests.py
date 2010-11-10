f50a5ba1f5c19b91477bfabc8a8e62d070314903
Thomas Lotze <thomas@thomas-lotze.de>
2010-11-10 07:42:51 +0100

    moved server layer into testing module

--- src/gocept/fssyncz2/tests.py
+++ src/gocept/fssyncz2/tests.py
@@ -1,10 +1,7 @@
-import Lifetime
 import Missing
 import Testing.ZopeTestCase
-import Testing.ZopeTestCase.layer
+import gocept.fssyncz2.testing
 import pickle
-import random
-import time
 import unittest
 import urllib2
 import zope.testbrowser.browser
@@ -24,59 +21,12 @@ class Zope2ObjectsTest(unittest.TestCase):
                      repr(pickle.dumps({'foo': Missing.Value})))
 
 
-class Zope2FunctionalLayer(object):
-    """Make sure checkout doesn't fail with Zope2.
-
-    """
-
-    __bases__ = (Testing.ZopeTestCase.layer.ZopeLiteLayer,)
-    __name__ = 'layer'
-
-    def setUp(self):
-        Testing.ZopeTestCase.installProduct('Five')
-        Testing.ZopeTestCase.layer.ZopeLiteLayer.setUp()
-        Testing.ZopeTestCase.installPackage('gocept.fssyncz2')
-        # We need to access the Zope2 server from outside in order to see
-        # effects related to security proxies.
-        self.startZServer()
-
-    @property
-    def host(self):
-        return Testing.ZopeTestCase.utils._Z2HOST
-
-    @property
-    def port(self):
-        return Testing.ZopeTestCase.utils._Z2PORT
-
-    def startZServer(self):
-        if self.host is not None:
-            return
-        host = '127.0.0.1'
-        port = random.choice(range(55000, 55500))
-        from Testing.ZopeTestCase.threadutils import setNumberOfThreads
-        setNumberOfThreads(5)
-        from Testing.ZopeTestCase.threadutils import QuietThread, zserverRunner
-        t = QuietThread(target=zserverRunner, args=(host, port, None))
-        t.setDaemon(1)
-        t.start()
-        time.sleep(0.1)  # Sandor Palfy
-
-        Testing.ZopeTestCase.utils._Z2HOST = host
-        Testing.ZopeTestCase.utils._Z2PORT = port
-
-    def tearDown(self):
-        Lifetime.shutdown(0, fast=1)
-
-
-functional_layer = Zope2FunctionalLayer()
-
-
 class CheckoutTests(Testing.ZopeTestCase.FunctionalTestCase):
     """Make sure checkout doesn't fail with Zope2.
 
     """
 
-    layer = functional_layer
+    layer = gocept.fssyncz2.testing.server_layer
 
     def setUp(self):
         Testing.ZopeTestCase.ZopeTestCase.setUp(self)
