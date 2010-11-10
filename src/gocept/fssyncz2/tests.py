053586ffb91bb67caa842dc1065e9c2c658cd03c
Thomas Lotze <thomas@thomas-lotze.de>
2010-11-10 21:40:13 +0100

    re #7986: added some tests that make sure of the order of folder entries and (non-folder) item attributes in the XML pickles

--- src/gocept/fssyncz2/tests.py
+++ src/gocept/fssyncz2/tests.py
@@ -1,8 +1,10 @@
 import Missing
+import OFS.SimpleItem
 import Testing.ZopeTestCase
 import doctest
 import gocept.fssyncz2.testing
 import pickle
+import random
 import unittest
 import urllib2
 import zope.testbrowser.browser
@@ -44,6 +46,70 @@ class CheckoutTests(Testing.ZopeTestCase.FunctionalTestCase):
                 'http://localhost:%s/folder/@@toFS.snarf' % self.layer.port)
         except urllib2.HTTPError, e:
             self.fail(e)
+
+
+class PickleOrderTest(Testing.ZopeTestCase.FunctionalTestCase):
+    """Make sure element order in XML pickles is kept stable.
+
+    """
+
+    layer = gocept.fssyncz2.testing.functional_layer
+
+    def setUp(self):
+        super(PickleOrderTest, self).setUp()
+        self.app['acl_users']._doAddUser('manager', 'asdf', ('Manager',), [])
+
+    def test_folder_entry_ordering(self):
+        for i in xrange(32):
+            self.app.manage_addFolder('folder')
+            creation_order = list('abcdef')
+            random.shuffle(creation_order)
+            for key in creation_order:
+                self.app['folder'].manage_addFile(key, 'foo')
+            response = self.publish(
+                '/folder/@@toFS.snarf', basic='manager:asdf')
+            dump_lines = response.getBody().splitlines(True)
+            self.assertEquals("""\
+name="folder"
+folder/@@Zope/Entries.xml
+name="a"
+name="b"
+name="c"
+name="d"
+name="e"
+name="f"
+folder/a
+folder/b
+folder/c
+folder/d
+folder/e
+folder/f
+""", ''.join(line[9:] for line in dump_lines
+             if ' folder/' in line or '<entry name=' in line))
+            self.app.manage_delObjects(['folder'])
+
+    def test_attribute_ordering(self):
+        for i in xrange(32):
+            self.app._setObject('object', OFS.SimpleItem.SimpleItem())
+            creation_order = list('abcdef')
+            random.shuffle(creation_order)
+            for key in creation_order:
+                setattr(self.app['object'], key, 'foo')
+            response = self.publish(
+                '/object/@@toFS.snarf', basic='manager:asdf')
+            dump_lines = response.getBody().splitlines(True)
+            self.assertEquals("""\
+        <item key="__ac_local_roles__">
+              <item key="test_user_1_">
+        <item key="_owner">
+          <key> <string>a</string> </key>
+          <key> <string>b</string> </key>
+          <key> <string>c</string> </key>
+          <key> <string>d</string> </key>
+          <key> <string>e</string> </key>
+          <key> <string>f</string> </key>
+""", ''.join([line for line in dump_lines if 'key' in line][1:]))
+            self.app.manage_delObjects(['object'])
 
 
 def test_suite():
