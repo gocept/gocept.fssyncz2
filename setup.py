1bd7515f055cd761d56ed9bbb32f23909f5c73f3
Wolfgang Schnerring <ws@gocept.com>
2011-01-31 11:36:39 +0100

    Add documentation of the package purpose

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ from setuptools import find_packages
 
 setup(name='gocept.fssyncz2',
       version='1.0dev',
+      author='gocept gmbh & co. kg',
+      author_email='mail@gocept.com',
+      description='zope.app.fssync integration for Zope2',
+      long_description=(
+        open('README.txt').read() +
+        '\n\n' +
+        open('CHANGES.txt').read()),
       packages=find_packages('src'),
       include_package_data=True,
       package_dir={'': 'src'},
+      license='ZPL 2.1',
       namespace_packages=['gocept'],
       zip_safe=False,
       install_requires=[
